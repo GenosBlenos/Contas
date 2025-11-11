a0fec918277d6f69f6355523ef839812b6405334
Matheus Ginna <107595564+GenosBlenos@users.noreply.github.com>
2025-11-11 09:39:20 -0300

    Otimização no espaço ocupado

--- src/includes/header.php
+++ src/includes/header.php
@@ -2,6 +2,8 @@
 require_once __DIR__ . '/config.php';
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
+    setlocale(LC_ALL, "pt_BR", "pt_BR.utf-8", "pt_BR.utf-8", "portuguese");
+    date_default_timezone_set("America/Sao_Paulo");
 }
 ?>
 <!DOCTYPE html>
